14fc39563b5ee183763d2c89bf25772117d67c0c
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-07 15:00:37 +0530

    Completed the week2 project task using one way

---
+++ Controllers/CustomConfigurationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadMapBasedProjects.Controllers
+{
+    public class CustomConfigurationController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            string? publickey = _configuration["keys:publicKey"];
+            string? mysecret = _configuration["keys:mysupersecret:mysecret"];
+            string? myothersecret = _configuration["keys:mysupersecret:myothersecret"];
+            return View(new{ publickey, mysecret, myothersecret });
+        }
+    }
+}
