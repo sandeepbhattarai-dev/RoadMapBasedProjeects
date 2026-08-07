552531fbe0df3d49adf35a42caea2617dc99af9f
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-07 12:02:21 +0530

    configuring to minimum MVC application

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-using RoadMapBasedProjeects.Models;
-using System.Diagnostics;
 
 namespace RoadMapBasedProjeects.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RoadMapBasedProjeects.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
