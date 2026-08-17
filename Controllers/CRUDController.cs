3072d5d63aee3af13269f942b179c6a688afa8c9
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-17 15:14:48 +0530

    Week3, completed the create and adding the style portion

---
+++ Controllers/CRUDController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoadMapBasedProjects.Models;
+using System.Collections.Generic;
+
+namespace RoadMapBasedProjects.Controllers
+{
+  public class CRUDController : Controller
+  {
+
+    public List<User> _user = new List<User>
+      {
+      new User { Id = 1, Name="Ron"},
+      new User { Id = 2, Name="Roy"}
+    };
+
+    public IActionResult Index()
+    {
+      return View(_user);
+    }
+
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+    //  [HttpPost]
+    //  public IActionResult Create(string country = "don", string capital = "key")
+    //  {
+
+    //  }
+
+    //  public IActionResult Edit(string country)
+    //  {
+
+
+    //  }
+
+    //  [HttpPost]
+    //  public IActionResult Edit(string country = "don", string capital = "key")
+    //  {
+
+    //  }
+
+
+    //}
+  }
+}
