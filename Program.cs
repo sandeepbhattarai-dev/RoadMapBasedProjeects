3aceb153054117602118d8958cca33f6028c7304
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-19 12:50:58 +0530

    making minimum, deleted wwwroot and changed the program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,6 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
