c6b580ccbd452bf1dfb84c2faff14f15bbf1d583
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-19 16:17:07 +0530

    saving

--- Program.cs
+++ Program.cs
@@ -6,19 +6,14 @@ var builder = WebApplication.CreateBuilder(
   }
  );
 
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
