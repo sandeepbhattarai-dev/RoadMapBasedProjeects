446a3119eebe4f2c9c8c2383a8d63392234b3621
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-08-22 18:13:11 +0530

    savving

--- DTOs/RegistrationDTO.cs
+++ DTOs/RegistrationDTO.cs
@@ -40,12 +40,12 @@ namespace RoadMapBasedProjects.DTOs
     public string? ConfirmPassword { get; set; }
 
     [Required(ErrorMessage = "Please select a gender.")]
-    public Gender? Gender { get; set; }
+    public int Gender { get; set; }
 
     [Required(ErrorMessage = "Please accept the terms and conditions.")]
     public bool IsTermsAgreed { get; set; }
 
 
-    public List<int> HobbyId { get; set; } = new List<int>();
+    public List<string> Hobbys { get; set; } = new List<string>();
   }
 }
